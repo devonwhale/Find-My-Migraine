89af4938e09430fabbd4a2501b5339c4d9930dcc
George Bell <george@cattlebell.com>
2015-02-16 09:11:20 +0000

    Updated documentation of DailyActivity
    Added some doumentation to the DailyActivity
    Removed some old, commented out code from DailyActivity

--- app/src/main/java/uk/ac/bradford/findmymigraine/DailyActivity.java
+++ app/src/main/java/uk/ac/bradford/findmymigraine/DailyActivity.java
@@ -17,17 +17,8 @@ public class DailyActivity extends ActionBarActivity {
         setContentView(R.layout.activity_daily);
 
         Button but_Sleep = (Button) findViewById(R.id.btnSleep);
-        /*but_Sleep.setOnClickListener(new View.OnClickListener() {
-            /**
-             * Moves to the Daily Activity Recorder
-             * @param v The current view
-             *//*
-            @Override
-            public void onClick(View v) {
-                Intent mv_daily = new Intent(getApplicationContext(), SleepActivity.class);
-                startActivity(mv_daily);
-            }
-        });*/
+        
+        //Moves to the Sleep Diary
         but_Sleep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

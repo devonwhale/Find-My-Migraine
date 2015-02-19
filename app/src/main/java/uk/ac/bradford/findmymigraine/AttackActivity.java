1c096a1039d64435a266e5943fa27eaae2e5cc5b
George Bell <george@cattlebell.com>
2015-02-19 08:25:13 +0000

    Updated documentation

--- app/src/main/java/uk/ac/bradford/findmymigraine/AttackActivity.java
+++ app/src/main/java/uk/ac/bradford/findmymigraine/AttackActivity.java
@@ -8,13 +8,18 @@ import android.view.MenuItem;
 
 public class AttackActivity extends ActionBarActivity {
 
+    /**
+    * Creates the view on the android device
+    * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attack);
     }
 
-
+    /**
+     * Creates the options menu
+     * */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -22,6 +27,9 @@ public class AttackActivity extends ActionBarActivity {
         return true;
     }
 
+    /**
+     * Deals with items  being selected in the options menu
+     * */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will

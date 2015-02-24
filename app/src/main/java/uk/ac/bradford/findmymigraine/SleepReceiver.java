aadca0c26cd5788431dc85fdb816af7c8bba1e63
devonwhale <george@cattlebell.com>
2015-02-24 11:13:04 +0000

    Fixed TimePicker Data not being saved Fixed toasts not appearing after setting the time Fixed the time not appearing on the "Click here" text

---
+++ app/src/main/java/uk/ac/bradford/findmymigraine/SleepReceiver.java
@@ -0,0 +1,17 @@
+package uk.ac.bradford.findmymigraine;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by George on 24/02/2015.
+ */
+public class SleepReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+}
